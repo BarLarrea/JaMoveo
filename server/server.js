84c33ea7e5ae0e889d5b8594d8f4079da1c7cdba
Bar Larrea <barlarrea1@gmail.com>
2025-06-21 11:55:35 +0300

    Basic server set-up

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import exprress from "express"
+import http from "http"
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Importing necessary modules
+const app = exprress()
+const server = http.createServer(app)
+
+const PORT = process.env.PORT || 5000;
+
+// Starting the server
+app.get("/", (req, res) => {
+    res.send("Hello JaMoveo")
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
