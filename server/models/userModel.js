85f72b74cdac7a6e4f305346d00a0880ab288891
Bar Larrea <barlarrea1@gmail.com>
2025-06-21 16:56:32 +0300

    Define user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,2 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        isSinger: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+        instrument: {
+            type: String,
+            required: function () {
+                return !this.isSinger;
+            } // Only required if not a singer
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6 // Minimum length for password
+        }
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
